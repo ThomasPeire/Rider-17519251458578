740ebe7d599bdb601b87d8a982bed149d4fa66e8
Thomas Peire <thomas.peire@outlook.com>
2024-03-12 13:13:22 +0100

    reproduction

---
+++ AspNetMvc/App_Start/BundleConfig.cs
@@ -0,0 +1,16 @@
+﻿using System.Web.Optimization;
+
+namespace AspNetMvc
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include("~/Assets/Scripts/jquery-{version}.js"));
+            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include("~/Assets/Scripts/jquery.validate*"));
+            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include("~/Assets/Scripts/modernizr-*"));
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include("~/Assets/Scripts/bootstrap.js"));
+            bundles.Add(new StyleBundle("~/Assets/Styles/css").Include("~/Assets/Styles/bootstrap.css", "~/Assets/Styles/site.css"));
+        }
+    }
+}
